b302b552d8caa2b6f3bed217618da3a5004ecd39
Kacper Kafara <kacperkafara@gmail.com>
2022-06-09 07:37:04 +0200

    refact: fix whitespace in ga::probe.rs

--- src/ga/probe.rs
+++ src/ga/probe.rs
@@ -4,12 +4,12 @@ pub mod stdout_probe;
 pub mod csv_probe;
 
 pub trait Probe {
-  fn on_start(&mut self)                      { /* defaults to noop */ }
-  fn on_new_best(&mut self, individual: &Individual)                   { /* defaults to noop */ }
-  fn on_mutation(&mut self, before: &Individual, after: &Individual)                   { /* defaults to noop */ }
-  fn on_new_generation(&mut self)             { /* defaults to noop */ }
-  fn on_best_fit_in_generation(&mut self, individual: &Individual)     { /* defaults to noop */ }
-  fn on_iteration_start(&mut self, iteration: usize)            { /* defaults to noop */ }
-  fn on_iteration_end(&mut self, iteration: usize)              { /* defaults to noop */ }
+  fn on_start(&mut self) { /* defaults to noop */ }
+  fn on_new_best(&mut self, individual: &Individual) { /* defaults to noop */ }
+  fn on_mutation(&mut self, before: &Individual, after: &Individual) { /* defaults to noop */ }
+  fn on_new_generation(&mut self) { /* defaults to noop */ }
+  fn on_best_fit_in_generation(&mut self, individual: &Individual) { /* defaults to noop */ }
+  fn on_iteration_start(&mut self, iteration: usize) { /* defaults to noop */ }
+  fn on_iteration_end(&mut self, iteration: usize) { /* defaults to noop */ }
 }
 
