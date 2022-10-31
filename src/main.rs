bd469607b077a5620dccd711bcf87c15e90ce1fd
Kacper Kafara <kacperkafara@gmail.com>
2022-10-31 16:48:40 +0100

    Test new API in main

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,12 @@ mod aco;
 mod ga;
 mod pso;
 mod ff;
-mod ganew;
 mod examples;
 
-extern crate core;
-
-use aco::probe::CsvProbe;
-use aco::AntSystemCfg;
 use ga::*;
-use pso::*;
-use ff::*;
-use ff::auxiliary::*;
-use ff::probe::console_probe::ConsoleProbe;
 
 
 fn main() {
+  ga::Builder::new().build();
 }
 
