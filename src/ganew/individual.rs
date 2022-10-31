927631b3203bffb4b6e1e279fcef02906781c6b4
Kacper Kafara <kacperkafara@gmail.com>
2022-10-31 13:46:46 +0100

    Remove redundant files

--- src/ganew/individual.rs
+++
@@ -1,7 +0,0 @@
-/// This trait describes common behaviur of individual (chromosome) encoding.
-///
-/// It can be impelmented on user's custom struct, so it can be consumed by
-/// our algorithms implementations.
-pub trait Individual {
-	// pub fn mutate
-}
