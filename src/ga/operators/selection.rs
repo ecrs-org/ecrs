082ffd303e5e8b71dab8f7f29a527537e86ebad8
Kacper Kafara <kacperkafara@gmail.com>
2022-11-12 00:27:47 +0100

    feat: add first type of rank selection  (#71)
    ## Description
    
    Should be merged only after
    
    * #69 
    
    is merged.
    
    Added missing selection operator. 
    
    This is one of two popular implementations. Second one has additional
    `r` parameter, which is used to decide which of random selected
    individuals to use as a parent. I left the second implementation behind
    for now, as presence of additional parameter raises some problems with
    our current approach to operator implementations. Namely we can't just
    add additional param to function declaration as it would break existing
    selection operator API in GA implementation (how do we decide whether to
    pass 2 or 3 params to operator in runtime?). One possible solution might
    be to implement operators as traits (concrete implementations as structs
    with `FnMut` trait implemented). Such approach would not only allow for
    operator parameterization but also for some kind of function overload
    which is not present in Rust.
    
    ## Linked issues
    
    Closes #37 
    
    ## Important implementation details
    
    N/A

--- src/ga/operators/selection.rs
+++ src/ga/operators/selection.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 use crate::ga::individual::{ChromosomeWrapper, Chromosome};
 
 pub fn roulette_wheel<T: Chromosome, S: ChromosomeWrapper<T>>(population: &Vec<S>, count: usize) -> Vec<&S> {
@@ -31,6 +33,30 @@ pub fn random<T: Chromosome, S: ChromosomeWrapper<T>>(population: &Vec<S>, count
 
 	for i in indices {
 		selected.push(&population[i]);
+	}
+
+	selected
+}
+
+pub fn rank<T: Chromosome, S: ChromosomeWrapper<T>>(population: &Vec<S>, count: usize) -> Vec<&S> {
+	// TODO: Second implementation with r parameter
+
+	let mut selected: Vec<&S> = Vec::with_capacity(count);
+
+	let population_len = population.len();
+	for _ in 0..count {
+		// TODO: Consider creating two random index permutations and then iterating over them
+		// instead of N times using random.
+		let p1 = & population[rand::thread_rng().gen_range(0..population_len)];
+		let p2 = &population[rand::thread_rng().gen_range(0..population_len)];
+
+		selected.push(
+			if p1.get_fitness() >= p2.get_fitness() {
+				p1
+			} else {
+				p2
+			}
+		)
 	}
 
 	selected
