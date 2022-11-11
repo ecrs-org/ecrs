6a27e6073a2587ef8c808e38fd4336979ee21346
Kacper Kafara <kacperkafara@gmail.com>
2022-11-12 00:39:35 +0100

    feat: add two point crossover operator (#77)
    ## Description
    
    Should be merged only after 
    
    * #75 
    
    is merged.
    
    Added two point crossover operator implementation.
    
    ## Linked issues
    
    Resolves #41
    
    ## Important implementation details
    
    <!-- if any, optional section -->

--- src/ga/operators/crossover.rs
+++ src/ga/operators/crossover.rs
@@ -16,23 +16,67 @@ where
 	ChWrapperT: ChromosomeWrapper<ChT>,
 	GeneT: Copy
 {
-	fn apply(&mut self, parent1: &ChWrapperT, parent2: &ChWrapperT) -> (ChWrapperT, ChWrapperT) {
-		let chromosome_len = parent1.get_chromosome().len();
+	fn apply(&mut self, parent_1: &ChWrapperT, parent_2: &ChWrapperT) -> (ChWrapperT, ChWrapperT) {
+		let chromosome_len = parent_1.get_chromosome().len();
 		let cut_point = rand::thread_rng().gen_range(0..chromosome_len);
 
-		let mut child1 = ChWrapperT::new();
-		let mut child2 = ChWrapperT::new();
+		let mut child_1 = ChWrapperT::new();
+		let mut child_2 = ChWrapperT::new();
 
 		for locus in 0..cut_point {
-			child1.get_chromosome_mut().push(parent1.get_chromosome()[locus]);
-			child2.get_chromosome_mut().push(parent2.get_chromosome()[locus]);
+			child_1.get_chromosome_mut().push(parent_1.get_chromosome()[locus]);
+			child_2.get_chromosome_mut().push(parent_2.get_chromosome()[locus]);
 		}
 
 		for locus in cut_point..chromosome_len {
-			child1.get_chromosome_mut().push(parent2.get_chromosome()[locus]);
-			child2.get_chromosome_mut().push(parent1.get_chromosome()[locus]);
+			child_1.get_chromosome_mut().push(parent_2.get_chromosome()[locus]);
+			child_2.get_chromosome_mut().push(parent_1.get_chromosome()[locus]);
 		}
 
-		(child1, child2)
+		(child_1, child_2)
+	}
+}
+
+pub struct TwoPoint;
+
+impl<GeneT, ChT, ChWrapperT> CrossoverOperator<ChT, ChWrapperT> for TwoPoint
+where
+	ChT: Chromosome + Index<usize, Output = GeneT> + Push<GeneT, PushedOut = Nothing>,
+	ChWrapperT: ChromosomeWrapper<ChT>,
+	GeneT: Copy
+{
+	fn apply(&mut self, parent_1: &ChWrapperT, parent_2: &ChWrapperT) -> (ChWrapperT, ChWrapperT) {
+		assert_eq!(parent_1.get_chromosome().len(), parent_2.get_chromosome().len(), "Parent chromosome length must match");
+
+		let chromosome_len = parent_1.get_chromosome().len();
+
+		let cut_points = (rand::thread_rng().gen_range(0..chromosome_len), rand::thread_rng().gen_range(0..chromosome_len));
+
+		let (cut_point_1, cut_point_2) = if cut_points.0 <= cut_points.1 {
+			(cut_points.0, cut_points.1)
+		} else {
+			(cut_points.1, cut_points.0)
+		};
+
+		let mut child_1 = ChWrapperT::new();
+		let mut child_2 = ChWrapperT::new();
+
+		for locus in 0..cut_point_1 {
+			child_1.get_chromosome_mut().push(parent_1.get_chromosome()[locus]);
+			child_2.get_chromosome_mut().push(parent_2.get_chromosome()[locus]);
+		}
+
+		for locus in cut_point_1..cut_point_2 {
+			child_1.get_chromosome_mut().push(parent_2.get_chromosome()[locus]);
+			child_2.get_chromosome_mut().push(parent_1.get_chromosome()[locus]);
+
+		}
+
+		for locus in cut_point_2..chromosome_len {
+			child_1.get_chromosome_mut().push(parent_1.get_chromosome()[locus]);
+			child_2.get_chromosome_mut().push(parent_2.get_chromosome()[locus]);
+		}
+
+		(child_1, child_2)
 	}
 }
