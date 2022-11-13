7b412ea24762687a5fe866b772f43d18bc011b4e
Kacper Kafara <kacperkafara@gmail.com>
2022-11-13 15:25:14 +0100

    Restore rastrigin example

--- src/ga/example.rs
+++ src/ga/example.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use rand::{Rng, thread_rng};
 use rand::distributions::{Distribution, Uniform};
 
@@ -9,7 +10,7 @@ pub fn sum_of_squares_fitness(individual: &Individual<Vec<f64>>) -> f64 {
 	10.0 * f64::exp(-sum_of_squares(individual.get_chromosome()))
 }
 
-pub fn sum_of_squares(chromosome: &[f64]) -> f64 {
+fn sum_of_squares(chromosome: &[f64]) -> f64 {
 	chromosome.iter().map(|v| v * v).sum()
 }
 
@@ -38,4 +39,27 @@ pub fn quadratic_population_factory(population_size: usize) -> Vec<Individual<Ve
     });
   }
   population
+}
+
+pub fn rastrigin_fitness(individual: &Individual<Vec<f64>>) -> f64 {
+	5.0 * f64::exp(-rastrigin(individual.get_chromosome()))
+}
+
+fn rastrigin(chromosome: &[f64]) -> f64 {
+	10.0 * chromosome.len() as f64 + chromosome.iter().fold(0.0, |sum, x| {
+		sum + x.powi(2) - 10.0 * (2.0 * std::f64::consts::PI * x).cos()
+	})
+}
+
+pub fn rastrigin_population_factory(population_size: usize) -> Vec<Individual<Vec<f64>>> {
+	let mut population: Vec<Individual<Vec<f64>>>  = Vec::with_capacity(population_size);
+
+	let domain = rand::distributions::Uniform::from(-5.12..5.12);
+
+	for _ in 0..population_size {
+		let chromosome = rand::thread_rng().sample_iter(domain).take(3).into_iter().collect_vec();
+		population.push(Individual { chromosome, fitness: f64::MAX })
+	}
+
+	population
 }
