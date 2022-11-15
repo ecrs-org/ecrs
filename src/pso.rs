7a49d5e9e73e85e81aa5725080eb0d579c750449
Kacper Kafara <kacperkafara@gmail.com>
2022-11-15 18:30:23 +0100

    FIX PSO EXAMPLES

--- src/pso.rs
+++ src/pso.rs
@@ -61,18 +61,19 @@ impl Default for PSOAlgorithmCfg {
 /// Struct used for running PSO algorithm
 /// # Usage example:
 /// ```rust
-/// # use ecrs::pso::{PSOAlgorithmCfg, PSOAlgorithm};
-/// # use ecrs::pso::probe::console_probe::ConsoleProbe;
-///
-/// let config = PSOAlgorithmCfg {
-///     dimensions: 3,
-///     iterations: 1000,
-///     probe: Box::new(ConsoleProbe::new()),
-///     ..PSOAlgorithmCfg::default()
-/// };
-///
-/// let mut algorithm = PSOAlgorithm::new(config);
-///
+/// # use ecrs::pso::{builder::PSOAlgorithmBuilder, self};
+/// let iterations = 2000;
+/// let console_probe = Box::new(pso::probe::console_probe::ConsoleProbe::new());
+/// let csv_probe = Box::new(pso::probe::csv_probe::CsvProbe::new("pso_example.csv"));
+/// let json_probe = Box::new(pso::probe::json_probe::JsonProbe::new("pso_example.json"));
+/// let probes : Vec<Box<dyn pso::probe::Probe>> = vec![console_probe, csv_probe, json_probe];
+/// let multi_probe = Box::new(pso::probe::multi_probe::MultiProbe::new(probes));
+/// let iteration_count_probe = Box::new(pso::probe::iteration_count_probe::IterationCountProbe::new(multi_probe, 50, iterations));
+/// let mut algorithm = PSOAlgorithmBuilder::new()
+///     .set_dimensions(3)
+///     .set_iterations(iterations)
+///     .set_probe(iteration_count_probe)
+///     .build();
 /// algorithm.run();
 /// ```
 pub struct PSOAlgorithm {
