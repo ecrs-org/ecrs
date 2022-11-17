0ecbe0b09f66fb4e1bc7a5f35e36563022412ea5
Kacper Kafara <kacperkafara@gmail.com>
2022-11-17 17:31:23 +0100

    [GA] Population initialization (#138)
    ## Description
    
    * Added population module with two simple populations generators:
    
      * `RandomPoints`
      * `BitStrings` 
    
    * Modified GA's builder interface to work with these.
    * Updated implementation of GA
    
    
    ## Linked issues
    
    Resolves #131 
    Resolves #139 
    Resolves #140 
    
    ## Important implementation details
    
    <!-- if any, optional section -->

---
+++ src/ga/population.rs
@@ -0,0 +1,153 @@
+use std::ops::Range;
+use itertools::Itertools;
+use rand::Rng;
+
+use super::individual::{Chromosome, ChromosomeWrapper};
+
+/// Implement this trait in order to provide custom population generator
+/// and feed it to an solver.
+pub trait PopulationGenerator<T: Chromosome, S: ChromosomeWrapper<T>> {
+	fn generate(&self, count: usize) -> Vec<S>;
+}
+
+/// Implements [PopulationGenerator] trait. Can be used with genetic algorithm.
+///
+/// Generates vector of random points from R^(dim) space within passed domain restrictions.
+pub struct RandomPoints {
+	dim: usize,
+	restrictions: Vec<(f64, f64)>
+}
+
+impl RandomPoints {
+	/// Returns [RandomPoints] population generator
+	///
+	/// ### Arguments
+	///
+	/// * `dim` -- Dimension of the sampling space
+	/// * `restrictions` -- Ranges for coordinates
+	pub fn new(dim: usize, restrictions: Vec<Range<f64>>) -> Self {
+		assert!(dim > 0, "Space dimension must be > 0");
+		assert_eq!(dim, restrictions.len(), "Number of restrictions must match dimension of sampled space");
+
+		RandomPoints {
+			dim,
+			restrictions: restrictions.into_iter()
+																.map(|range| (range.end - range.start, range.start))
+																.collect_vec()
+		}
+	}
+}
+
+impl<S> PopulationGenerator<Vec<f64>, S> for RandomPoints
+where
+	S: ChromosomeWrapper<Vec<f64>>
+{
+	/// Generates vector of `count` random points from R^(dim) space within passed domain restrictions
+	///
+	/// ### Arguments
+	///
+	/// * `count` -- Number of points to generate
+	fn generate(&self, count: usize) -> Vec<S> {
+		// FIXME: Sampling from such short interval may cause some f64 values to be more unlikely...
+		let distribution = rand::distributions::Uniform::from(0.0..1.0);
+
+		let mut population: Vec<S> = Vec::with_capacity(count);
+		let rng = &mut rand::thread_rng();
+
+		for _ in 0..count {
+			let mut point: Vec<f64> = Vec::with_capacity(self.dim);
+			for restriction in &self.restrictions {
+				point.push(restriction.0 * rng.sample(distribution) + restriction.1);
+			}
+			population.push(S::from(point));
+		}
+
+		population
+	}
+}
+
+/// Implements [PopulationGenerator] trait. Can be used with genetic algorithm.
+///
+/// Generates vector of random bit-strings.
+pub struct BitStrings {
+	dim: usize,
+}
+
+impl BitStrings {
+	/// Returns [BitString] population generator
+	///
+	/// ### Arguments
+	///
+	/// * `dim` -- Dimension of the sampling space
+	pub fn new(dim: usize) -> Self {
+		assert!(dim > 0, "Space dimension must be > 0");
+		BitStrings { dim }
+	}
+}
+
+impl<S> PopulationGenerator<Vec<bool>, S> for BitStrings
+where
+	S: ChromosomeWrapper<Vec<bool>>
+{
+	/// Generates vector of `count` random bitstrings
+	///
+	/// ### Arguments
+	///
+	/// * `count` -- Number of bitstrings to generate
+	fn generate(&self, count: usize) -> Vec<S> {
+		let mut population: Vec<S> = Vec::with_capacity(count);
+
+		let distr = rand::distributions::Uniform::from(0.0..1.0);
+		let rng = &mut rand::thread_rng();
+
+		for _ in 0..count {
+			population.push(S::from(rng.sample_iter(distr).take(self.dim).map(|v| v < 0.5).collect_vec()));
+		}
+
+		population
+	}
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::ga::individual::ChromosomeWrapper;
+
+    use super::{RandomPoints, PopulationGenerator, BitStrings};
+
+	#[test]
+	fn points_have_appropriate_len() {
+		let dim = 4;
+		let gen = RandomPoints::new(dim, vec![(0.0..2.0), (-1.0..1.0), (3.0..10.0), (-5.0..-4.0)]);
+		let points: Vec<crate::ga::Individual<Vec<f64>>> = gen.generate(30);
+
+		for p in points {
+			assert_eq!(p.get_chromosome().len(), dim)
+		}
+	}
+
+	#[test]
+	fn points_follow_restrictions() {
+		let dim = 4;
+		let restrictions = vec![(0.0..2.0), (-1.0..1.0), (3.0..10.0), (-5.0..-4.0)];
+		let gen = RandomPoints::new(dim, restrictions.clone());
+		let points: Vec<crate::ga::Individual<Vec<f64>>> = gen.generate(30);
+
+		for p in points {
+			for (v, res) in std::iter::zip(p.get_chromosome(), &restrictions) {
+				assert!(res.contains(v))
+			}
+		}
+	}
+
+	#[test]
+	fn bistrings_have_appropriate_len() {
+		let dim = 30;
+		let gen = BitStrings::new(dim);
+		let points: Vec<crate::ga::Individual<Vec<bool>>> = gen.generate(30);
+
+		for p in points {
+			assert_eq!(p.get_chromosome().len(), dim)
+		}
+	}
+}
