9c33f207c0f47850f5b86d53927c3fe88fb77b9a
Kacper Kafara <kacperkafara@gmail.com>
2022-11-20 22:46:01 +0100

    test: mock tests for `mutation` module (#159)
    <!-- If applicable - remeber to add the PR to the EA Rust project (ONLY
    IF THERE IS NO LINKED ISSUE) -->
    
    ## Description
    
    Added some very simple (mock) tests for `mutation` module
    
    ## Linked issues
    
    Resolves #144

--- src/ga/operators/mutation.rs
+++ src/ga/operators/mutation.rs
@@ -5,12 +5,13 @@ use crate::ga::{individual::Chromosome, Individual};
 /// This trait defines common behaviour for mutation operators.
 /// You can implement this trait to provide your custom crossover operator to the GA.
 pub trait MutationOperator<T: Chromosome> {
+
 	/// Mutates provided solution in place
 	///
 	/// ## Arguments
 	///
 	/// * `individual` - mutable reference to to-be-mutated individual
-	fn apply(&mut self, indivudial: &mut Individual<T>);
+	fn apply(&self, indivudial: &mut Individual<T>);
 }
 
 /// # Identity Mutation Operator
@@ -29,5 +30,29 @@ impl Identity {
 }
 
 impl<T: Chromosome> MutationOperator<T> for Identity {
-	fn apply(&mut self, _indivudial: &mut Individual<T>) {}
+	fn apply(&self, _indivudial: &mut Individual<T>) {}
+}
+
+#[cfg(test)]
+mod tests {
+    use itertools::Itertools;
+    use rand::{Rng, distributions::Uniform};
+    use crate::ga::Individual;
+
+    use super::{Identity, MutationOperator};
+
+	#[test]
+	fn identity_does_not_change_chromosome() {
+		let chromosome = rand::thread_rng()
+			.sample_iter(Uniform::from(-1.0..1.0))
+			.take(30).collect_vec();
+
+		let mut individual = Individual { chromosome: chromosome.clone(), fitness: f64::default( )};
+
+		let identity_mutation = Identity;
+
+		identity_mutation.apply(&mut individual);
+
+		assert_eq!(chromosome, individual.chromosome);
+	}
 }
