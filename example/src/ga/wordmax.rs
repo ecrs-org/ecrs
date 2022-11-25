e6fb27b784c64dc5adcfda6cf0b1fc47942eb840
Kacper Kafara <kacperkafara@gmail.com>
2022-11-25 18:42:10 +0100

    Optimize imports

--- example/src/ga/wordmax.rs
+++ example/src/ga/wordmax.rs
@@ -1,4 +1,4 @@
-use ecrs::ga::{individual, Individual};
+use ecrs::ga::Individual;
 
 pub fn wordmax_fitness(individual: &Individual<Vec<bool>>) -> f64 {
   individual.chromosome_ref().iter().filter(|gene| **gene).count() as f64
