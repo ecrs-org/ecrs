f4e8549bd64dc9a9bbb4359fb3a3e14ce6b5092d
Kacper Kafara <kacperkafara@gmail.com>
2022-11-25 18:23:59 +0100

    Create presets for builder

--- src/ga/builder/presets.rs
+++ src/ga/builder/presets.rs
@@ -1,28 +1,33 @@
 use crate::ga::{
   operators::{
-    crossover::{CrossoverOperator, SinglePoint},
-    mutation::{Identity, MutationOperator},
-    selection::{SelectionOperator, Tournament},
+    crossover::SinglePoint,
+    mutation::Identity,
+    selection::Tournament,
   },
-  population::{PopulationGenerator, RandomPoints},
-  GAParams, Probe, StdoutProbe, FitnessFn, Individual, GeneticAlgorithm,
+  population::{RandomPoints, BitStrings},
+  StdoutProbe, FitnessFn, Individual, GeneticAlgorithm,
 };
 
 use super::GAConfigOpt;
 
-type RVC = Vec<f64>;
-type BC = Vec<bool>;
+type Rvc = Vec<f64>;
+type Bsc = Vec<bool>;
 
 pub struct RealValuedBuilder
 {
-  config: GAConfigOpt<RVC, Identity, SinglePoint, Tournament, RandomPoints, StdoutProbe>,
+  config: GAConfigOpt<Rvc, Identity, SinglePoint, Tournament, RandomPoints, StdoutProbe>,
+	dim: Option<usize>,
 }
 
 
 impl RealValuedBuilder
 {
-  pub fn set_selection_rate(mut self, selection_rate: f64) -> Self {
-    debug_assert!((0f64..=1f64).contains(&selection_rate));
+	pub(super) fn new() -> Self {
+		RealValuedBuilder { config: GAConfigOpt::new(), dim: None }
+	}
+
+  pub fn selection_rate(mut self, selection_rate: f64) -> Self {
+    assert!((0f64..=1f64).contains(&selection_rate));
     self.config.params = self.config.params.map(|mut params| {
       params.selection_rate = selection_rate;
       params
@@ -30,7 +35,7 @@ impl RealValuedBuilder
     self
   }
 
-  pub fn set_max_duration(mut self, max_duration: std::time::Duration) -> Self {
+  pub fn max_duration(mut self, max_duration: std::time::Duration) -> Self {
     self.config.params = self.config.params.map(|mut params| {
       params.max_duration = Some(max_duration);
       params
@@ -38,8 +43,8 @@ impl RealValuedBuilder
     self
   }
 
-  pub fn set_max_generation_count(mut self, max_gen_count: usize) -> Self {
-    debug_assert!(max_gen_count >= 1);
+  pub fn max_generations(mut self, max_gen_count: usize) -> Self {
+    assert!(max_gen_count >= 1);
     self.config.params = self.config.params.map(|mut params| {
       params.generation_upper_bound = max_gen_count;
       params
@@ -47,8 +52,8 @@ impl RealValuedBuilder
     self
   }
 
-  pub fn set_population_size(mut self, size: usize) -> Self {
-    debug_assert!(size > 0);
+  pub fn population_size(mut self, size: usize) -> Self {
+    assert!(size > 0);
     self.config.params = self.config.params.map(|mut params| {
       params.population_size = size;
       params
@@ -56,19 +61,96 @@ impl RealValuedBuilder
     self
   }
 
-	pub fn fitness_fn(mut self, fitness_fn: FitnessFn<Individual<RVC>>) -> Self {
+	pub fn dim(mut self, dim: usize) -> Self {
+		assert!(dim > 0, "Dimension of a problem must be > 0");
+		self.dim = Some(dim);
+		self
+	}
+
+	pub fn fitness_fn(mut self, fitness_fn: FitnessFn<Individual<Rvc>>) -> Self {
 		self.config.fitness_fn = Some(fitness_fn);
 		self
 	}
 
-	pub fn build(self) -> GeneticAlgorithm<RVC, Identity, SinglePoint, Tournament, RandomPoints, StdoutProbe> {
-		let Some(fitness_fn) = self.config.fitness_fn else {
-			panic!("Fitness function must be set");
-		};
+	pub fn build(self) -> GeneticAlgorithm<Rvc, Identity, SinglePoint, Tournament, RandomPoints, StdoutProbe> {
+		 if self.config.fitness_fn.is_none() {
+				panic!("Fitness function must be set");
+		 }
+
+		if self.dim.is_none() {
+			panic!("Problem dimension must be set");
+		}
 
 		GeneticAlgorithm::new(self.config.into())
 	}
 }
 
 
-pub struct BitStringBuilder {}
+pub struct BitStringBuilder {
+  config: GAConfigOpt<Bsc, Identity, SinglePoint, Tournament, BitStrings, StdoutProbe>,
+	dim: Option<usize>,
+}
+
+impl BitStringBuilder {
+	pub(super) fn new() -> Self {
+		BitStringBuilder { config: GAConfigOpt::new(), dim: None }
+	}
+
+  pub fn selection_rate(mut self, selection_rate: f64) -> Self {
+    assert!((0f64..=1f64).contains(&selection_rate));
+    self.config.params = self.config.params.map(|mut params| {
+      params.selection_rate = selection_rate;
+      params
+    });
+    self
+  }
+
+  pub fn max_duration(mut self, max_duration: std::time::Duration) -> Self {
+    self.config.params = self.config.params.map(|mut params| {
+      params.max_duration = Some(max_duration);
+      params
+    });
+    self
+  }
+
+  pub fn max_generations(mut self, max_gen_count: usize) -> Self {
+    assert!(max_gen_count >= 1);
+    self.config.params = self.config.params.map(|mut params| {
+      params.generation_upper_bound = max_gen_count;
+      params
+    });
+    self
+  }
+
+  pub fn population_size(mut self, size: usize) -> Self {
+    assert!(size > 0);
+    self.config.params = self.config.params.map(|mut params| {
+      params.population_size = size;
+      params
+    });
+    self
+  }
+
+	pub fn dim(mut self, dim: usize) -> Self {
+		assert!(dim > 0, "Dimension of a problem must be > 0");
+		self.dim = Some(dim);
+		self
+	}
+
+	pub fn fitness_fn(mut self, fitness_fn: FitnessFn<Individual<Bsc>>) -> Self {
+		self.config.fitness_fn = Some(fitness_fn);
+		self
+	}
+
+	pub fn build(self) -> GeneticAlgorithm<Bsc, Identity, SinglePoint, Tournament, BitStrings, StdoutProbe> {
+		 if self.config.fitness_fn.is_none() {
+				panic!("Fitness function must be set");
+		 }
+
+		if self.dim.is_none() {
+			panic!("Problem dimension must be set");
+		}
+
+		GeneticAlgorithm::new(self.config.into())
+	}
+}
