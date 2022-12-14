2c223e64491d38946a7105460ce9ac2685c05616
co012 <co123@interia.eu>
2022-12-14 15:53:37 +0100

    fix refactor artifact

--- examples/ff.rs
+++ examples/ff.rs
@@ -2,8 +2,6 @@ use ecrs::ff::auxiliary::*;
 use ecrs::ff::probe::console_probe::ConsoleProbe;
 use ecrs::ff::*;
 
-mod aco_tsp;
-
 fn main() {
   let mut alg = FireflyAlgorithm {
     config: FireflyAlgorithmCfg::default(),
