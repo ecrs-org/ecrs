9cddf78e2ed46aa420262d0b8068482fc0a30636
co012 <co123@interia.eu>
2022-12-16 15:09:49 +0100

    impl Fitness for functions

--- src/ga/operators/fitness.rs
+++ src/ga/operators/fitness.rs
@@ -3,3 +3,9 @@ use crate::ga::individual::Chromosome;
 pub trait Fitness<T: Chromosome> {
   fn apply(&self, chromosome: T) -> f64;
 }
+
+impl<T: Chromosome> Fitness<T> for fn(chromosome: T) -> f64 {
+  fn apply(&self, chromosome: T) -> f64 {
+    self(chromosome)
+  }
+}
