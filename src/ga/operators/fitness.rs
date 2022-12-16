7f1d42a64e5f7a1245842cf1515adc07859b2dcf
co012 <co123@interia.eu>
2022-12-16 15:11:26 +0100

    make fitness take a reference

--- src/ga/operators/fitness.rs
+++ src/ga/operators/fitness.rs
@@ -1,11 +1,11 @@
 use crate::ga::individual::Chromosome;
 
 pub trait Fitness<T: Chromosome> {
-  fn apply(&self, chromosome: T) -> f64;
+  fn apply(&self, chromosome: &T) -> f64;
 }
 
-impl<T: Chromosome> Fitness<T> for fn(chromosome: T) -> f64 {
-  fn apply(&self, chromosome: T) -> f64 {
+impl<T: Chromosome> Fitness<T> for fn(chromosome: &T) -> f64 {
+  fn apply(&self, chromosome: &T) -> f64 {
     self(chromosome)
   }
 }
