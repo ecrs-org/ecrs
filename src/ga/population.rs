c00c4350e9753cf456a9259f40823c1b9a84d366
co012 <co123@interia.eu>
2022-12-17 14:15:59 +0100

    add RNG to RandomPoints

--- src/ga/population.rs
+++ src/ga/population.rs
@@ -1,6 +1,7 @@
 use itertools::Itertools;
+use rand::prelude::ThreadRng;
 use rand::seq::SliceRandom;
-use rand::Rng;
+use rand::{thread_rng, Rng};
 use std::fmt::Debug;
 use std::ops::Range;
 
@@ -9,25 +10,49 @@ use super::{individual::Chromosome, Individual};
 /// Implement this trait in order to provide custom population generator
 /// and feed it to an solver.
 pub trait PopulationGenerator<T: Chromosome> {
-  fn generate(&self, count: usize) -> Vec<Individual<T>>;
+  fn generate(&mut self, count: usize) -> Vec<Individual<T>>;
 }
 
 /// Implements [PopulationGenerator] trait. Can be used with genetic algorithm.
 ///
 /// Generates vector of random points from R^(dim) space within passed domain constraints.
-pub struct RandomPoints {
+pub struct RandomPoints<R: Rng> {
   dim: usize,
   constraints: Vec<(f64, f64)>,
+  rng: R,
 }
 
-impl RandomPoints {
-  /// Returns [RandomPoints] population generator with given constraints
+impl RandomPoints<ThreadRng> {
+  /// Returns [RandomPoints] population generator with given constraints and default RNG
   ///
   /// ### Arguments
   ///
   /// * `dim` -- Dimension of the sampling space
   /// * `constraints` -- Ranges for coordinates
   pub fn with_constraints(dim: usize, constraints: Vec<Range<f64>>) -> Self {
+    Self::with_constraints_and_rng(dim, constraints, thread_rng())
+  }
+
+  /// Returns [RandomPoints] population generator with no explicit constraints and default RNG.
+  /// Points coords will be from range 0.0..1.0.
+  ///
+  /// ### Arguments
+  ///
+  /// * `dim` -- Dimension of the sampling space
+  pub fn new(dim: usize) -> Self {
+    Self::wih_rng(dim, thread_rng())
+  }
+}
+
+impl<R: Rng> RandomPoints<R> {
+  /// Returns [RandomPoints] population generator with given constraints and custom RNG
+  ///
+  /// ### Arguments
+  ///
+  /// * `dim` -- Dimension of the sampling space
+  /// * `constraints` -- Ranges for coordinates
+  /// * `rng` -- Random numbers generator
+  pub fn with_constraints_and_rng(dim: usize, constraints: Vec<Range<f64>>, rng: R) -> Self {
     assert!(dim > 0, "Space dimension must be > 0");
     assert_eq!(
       dim,
@@ -41,37 +66,39 @@ impl RandomPoints {
         .into_iter()
         .map(|range| (range.end - range.start, range.start))
         .collect_vec(),
+      rng,
     }
   }
 
-  /// Returns [RandomPoints] population generator with no explicit constraints.
+  /// Returns [RandomPoints] population generator with no explicit constraints and custom RNG.
   /// Points coords will be from range 0.0..1.0.
   ///
   /// ### Arguments
   ///
-  /// * `dim` - Dimension of the sampling space
-  pub fn new(dim: usize) -> Self {
+  /// * `dim` -- Dimension of the sampling space
+  /// * `rng` -- Random numbers generator
+  pub fn wih_rng(dim: usize, rng: R) -> Self {
     assert!(dim > 0, "Space dimension must be > 0");
     RandomPoints {
       dim,
       constraints: Vec::<(f64, f64)>::with_capacity(0),
+      rng,
     }
   }
 }
 
-impl PopulationGenerator<Vec<f64>> for RandomPoints {
+impl<R: Rng> PopulationGenerator<Vec<f64>> for RandomPoints<R> {
   /// Generates vector of `count` random points from R^(dim) space within passed domain constraints.
   /// If there were no constraints passed then the points coords are from range 0.0..1.0.
   ///
   /// ### Arguments
   ///
   /// * `count` -- Number of points to generate
-  fn generate(&self, count: usize) -> Vec<Individual<Vec<f64>>> {
+  fn generate(&mut self, count: usize) -> Vec<Individual<Vec<f64>>> {
     // FIXME: Sampling from such short interval may cause some f64 values to be more unlikely...
     let distribution = rand::distributions::Uniform::from(0.0..1.0);
 
     let mut population: Vec<Individual<Vec<f64>>> = Vec::with_capacity(count);
-    let rng = &mut rand::thread_rng();
 
     // We do not use Option to designate whether there are constraints or not
     // because using unwrap moves!
@@ -79,7 +106,7 @@ impl PopulationGenerator<Vec<f64>> for RandomPoints {
       for _ in 0..count {
         let mut point = Vec::<f64>::with_capacity(self.dim);
         for _ in 0..self.dim {
-          point.push(rng.sample(distribution));
+          point.push(self.rng.sample(distribution));
         }
         population.push(Individual::from(point));
       }
@@ -87,7 +114,7 @@ impl PopulationGenerator<Vec<f64>> for RandomPoints {
       for _ in 0..count {
         let mut point: Vec<f64> = Vec::with_capacity(self.dim);
         for restriction in &self.constraints {
-          point.push(restriction.0 * rng.sample(distribution) + restriction.1);
+          point.push(restriction.0 * self.rng.sample(distribution) + restriction.1);
         }
         population.push(Individual::from(point));
       }
@@ -121,7 +148,7 @@ impl PopulationGenerator<Vec<bool>> for BitStrings {
   /// ### Arguments
   ///
   /// * `count` -- Number of bitstrings to generate
-  fn generate(&self, count: usize) -> Vec<Individual<Vec<bool>>> {
+  fn generate(&mut self, count: usize) -> Vec<Individual<Vec<bool>>> {
     let mut population: Vec<Individual<Vec<bool>>> = Vec::with_capacity(count);
 
     let distr = rand::distributions::Uniform::from(0.0..1.0);
@@ -167,7 +194,7 @@ impl<GeneT: Copy + Debug + Sync + Send> PopulationGenerator<Vec<GeneT>> for Rand
   /// ### Arguments
   ///
   /// * `count` -- Number of random permutations to generate
-  fn generate(&self, count: usize) -> Vec<Individual<Vec<GeneT>>> {
+  fn generate(&mut self, count: usize) -> Vec<Individual<Vec<GeneT>>> {
     let mut population: Vec<Individual<Vec<GeneT>>> = Vec::with_capacity(count);
     let rng = &mut rand::thread_rng();
 
@@ -188,7 +215,8 @@ mod tests {
   #[test]
   fn points_have_appropriate_len() {
     let dim = 4;
-    let gen = RandomPoints::with_constraints(dim, vec![(0.0..2.0), (-1.0..1.0), (3.0..10.0), (-5.0..-4.0)]);
+    let mut gen =
+      RandomPoints::with_constraints(dim, vec![(0.0..2.0), (-1.0..1.0), (3.0..10.0), (-5.0..-4.0)]);
     let points: Vec<crate::ga::Individual<Vec<f64>>> = gen.generate(30);
 
     for p in points {
@@ -200,7 +228,7 @@ mod tests {
   fn points_follow_explicit_constraints() {
     let dim = 4;
     let constraints = vec![(0.0..2.0), (-1.0..1.0), (3.0..10.0), (-5.0..-4.0)];
-    let gen = RandomPoints::with_constraints(dim, constraints.clone());
+    let mut gen = RandomPoints::with_constraints(dim, constraints.clone());
     let points: Vec<crate::ga::Individual<Vec<f64>>> = gen.generate(30);
 
     for p in points {
@@ -215,7 +243,7 @@ mod tests {
     let dim = 30;
     let count = 100;
 
-    let gen = RandomPoints::new(dim);
+    let mut gen = RandomPoints::new(dim);
     let points: Vec<crate::ga::Individual<Vec<f64>>> = gen.generate(count);
 
     for p in points {
@@ -228,7 +256,7 @@ mod tests {
   #[test]
   fn bistrings_have_appropriate_len() {
     let dim = 30;
-    let gen = BitStrings::new(dim);
+    let mut gen = BitStrings::new(dim);
     let points: Vec<crate::ga::Individual<Vec<bool>>> = gen.generate(30);
 
     for p in points {
