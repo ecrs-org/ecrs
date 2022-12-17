6c5f70405d7781e8800a1e5b8f28d0fbbda73017
co012 <co123@interia.eu>
2022-12-17 20:10:27 +0100

    add test for AntSystemPU

--- src/aco/pheromone.rs
+++ src/aco/pheromone.rs
@@ -18,3 +18,35 @@ impl PheromoneUpdate for AntSystemPU {
     old_pheromone.scale(1.0 - evaporation_rate).add(delta_pheromone)
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use crate::aco::pheromone::{AntSystemPU, PheromoneUpdate};
+  use crate::aco::{FMatrix, Solution};
+
+  #[test]
+  fn check_ant_system_pu_with_example() {
+    let pheromone = FMatrix::from_column_slice(3, 3, &[0.0, 1.0, 2.0, 1.0, 0.0, 4.0, 2.0, 4.0, 0.0]);
+
+    let s1 = FMatrix::from_column_slice(3, 3, &[0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0]);
+    let s2 = FMatrix::from_column_slice(3, 3, &[0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0]);
+    let sols = [
+      Solution {
+        matrix: s1,
+        cost: 8.0,
+      },
+      Solution {
+        matrix: s2,
+        cost: 4.0,
+      },
+    ];
+
+    let mut pu = AntSystemPU;
+    let new_pheromone = pu.apply(&pheromone, &sols, 0.25);
+    let pheromone = vec![0.0, 1.125, 1.875, 1.125, 0.0, 3.375, 1.875, 3.375, 0.0];
+
+    for (p, p_exp) in new_pheromone.iter().zip(pheromone.iter()) {
+      assert_eq!(p, p_exp);
+    }
+  }
+}
