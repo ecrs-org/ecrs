67161c5179d5dd1a3cfc3e5b4eb0089a43a32fde
co012 <co123@interia.eu>
2022-12-18 20:19:09 +0100

    add best_policy module docs

--- src/aco/pheromone/best_policy.rs
+++ src/aco/pheromone/best_policy.rs
@@ -1,16 +1,36 @@
+//! Module contains implementations of policies on how to choose the best ant
+//!
+//! There are [PheromoneUpdate] implementations that make use of best found solution.
+//! This module was create to accommodate two main choosing policies
+//! * [OverallBest] - chooses the best in all iterations
+//! * [Iteration] - chooses the best in current iteration
+//!
 use crate::aco::{FMatrix, Solution};
 
+/// # Best Choosing Policy
+///
+/// This trait defines common behaviors of choosing the best ant.
+/// You can implement this trait to provide your own choosing policy
 pub trait BestPolicy {
+  /// Based on provided solutions, it updates it internal state.
+  ///
+  /// ## Arguments
+  /// * `solutions` - current iteration solutions.
   fn update_best(&mut self, solutions: &[Solution]);
 
+  /// Returns stored best pheromone trail
   fn get_best_pheromone(&self) -> &FMatrix;
 }
 
+/// # Iteration best choosing policy
+/// Implements [ChoosingPolicy].
+/// Chooses pheromone from current iteration best ant.
 pub struct IterationBest {
   best_pheromone: FMatrix,
 }
 
 impl IterationBest {
+  /// Crates a new instance of [IterationBest]
   pub fn new() -> Self {
     Self {
       best_pheromone: FMatrix::zeros(0, 0),
@@ -29,12 +49,16 @@ impl BestPolicy for IterationBest {
   }
 }
 
+/// # Overall best choosing policy
+/// Implements [ChoosingPolicy].
+/// Chooses pheromone from all past iteration best ant.
 pub struct OverallBest {
   best_pheromone: FMatrix,
   cost: f64,
 }
 
 impl OverallBest {
+  /// Crates a new instance of [OverallBest]
   pub fn new() -> Self {
     Self {
       best_pheromone: FMatrix::zeros(0, 0),
