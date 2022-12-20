bf7cc727dce7074642af3dcd4d808343c794a19e
co012 <co123@interia.eu>
2022-12-20 21:54:10 +0100

    add fitness docs

--- src/aco/fitness.rs
+++ src/aco/fitness.rs
@@ -1,15 +1,31 @@
+//! Ants paths grading.
+//!
+//! Contains trait [Fitness] that should be implemented for grading structure.
+//! Higher fitness means beater path.
+//! <b> Fitness must be grater or equal 0 </b>
 use crate::aco::FMatrix;
 use itertools::Itertools;
 
+/// # Fitness
+///
+/// Trait defining method needed for grading.
 pub trait Fitness {
+  /// Calculates fitness of given path.
+  ///
+  ///  ## Arguments
+  /// * `path` - path in sequence of vertices form.
   fn apply(&mut self, path: &[usize]) -> f64;
 }
 
+/// # Canonical Fitness
+///
+/// Calculates fitness as 1.0 / path_cost
 pub struct CanonicalFitness {
   weights: FMatrix,
 }
 
 impl CanonicalFitness {
+  /// Creates a new instance of [CanonicalFitness].
   pub fn new(weights: FMatrix) -> Self {
     Self { weights }
   }
