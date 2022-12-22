aa89497f1c0844ac0fcb4debc231805221817aae
Kacper Kafara <kacperkafara@gmail.com>
2022-12-22 15:07:54 +0100

    feat: `replacement` - improve interface & some fixes (#264)
    ## Description
    
    So I've just merged #263 skipping review process 🤦🏻‍♂️ and had to
    immediately introduce some fixes;
    
    1. I decided to take ownership of population & children, so that I can
    modify it in the operator and avoid necessity of creating a copy.
    2. Added `requires_children_fitness` method which indicated whether
    children's fitness should be evaluated.
    3. Fixed `BothParents` implementation

--- src/ga/operators/replacement.rs
+++ src/ga/operators/replacement.rs
@@ -18,11 +18,43 @@ use crate::ga::{individual::Chromosome, Individual};
 /// be considered an undefined behaviour. We'll work toward improving this case in the future.
 pub trait ReplacementOperator<T: Chromosome> {
   /// Merges `children` - output of crossover operator with current population.
-  /// In current implementation (and design) the merge happens in place (where possible)
-  /// to avoid necessity of copying whole population each time the operator is called.
-  /// This might be a premature optimization and we will consider benchmarking this approach
-  /// and possibly making API more intuitive by making this function return the merged
-  /// population instead of modyfying in-place.
+  ///
+  /// **NOTE**: In current implementation, all library-implemented operators assume that
+  /// at indices i, i+1 in `population` collection there are parents of children i, i+1
+  /// from `children` collection. Any violation of this invariant may lead to bugs - it can
+  /// be considered an undefined behaviour. We'll work toward improving this case in the future.
+  ///
+  /// ### Arguements
+  ///
+  /// * `population` - Original population, input to the crossover phase.
+  /// This collection should be modified in place by the operator.
+  /// * `children` - Result of the crossover phase.
+  fn apply(&self, population: Vec<Individual<T>>, children: Vec<Individual<T>>) -> Vec<Individual<T>>;
+
+  /// Returns `true` when the operator requries children to possess valid fitness values.
+  ///
+  /// Default implementation returns `false`
+  fn requires_children_fitness(&self) -> bool {
+    true
+  }
+}
+
+/// # BothParents replacement operator
+///
+/// This struct implements [ReplacementOperator] trait and can be used with genetic algorithm.
+///
+/// It works simply by replacing parents with their children. In effect, each individual
+/// only gets to breed once.
+pub struct BothParents;
+
+impl BothParents {
+  pub fn new() -> Self {
+    Self
+  }
+}
+
+impl<T: Chromosome> ReplacementOperator<T> for BothParents {
+  /// Works simply by replacing parents with their children
   ///
   /// **NOTE**: In current implementation, all library-implemented operators assume that
   /// at indices i, i+1 in `population` collection there are parents of children i, i+1
@@ -34,28 +66,16 @@ pub trait ReplacementOperator<T: Chromosome> {
   /// * `population` - Original population, input to the crossover phase.
   /// This collection should be modified in place by the operator.
   /// * `children` - Result of the crossover phase
-  fn apply(&self, population: &mut [Individual<T>], children: &[Individual<T>]);
-}
-
-/// # BothParents replacement operator
-///
-/// This struct implements [ReplacementOperator] trait and can be used with genetic algorithm.
-///
-/// It works simply by replacing parents with their children. In effect, each individual
-/// only gets to breed once.
-///
-/// This operator has the same effect as [Noop].
-pub struct BothParents;
-
-impl BothParents {
-  pub fn new() -> Self {
-    Self
-  }
-}
-
-impl<T: Chromosome> ReplacementOperator<T> for BothParents {
   #[inline(always)]
-  fn apply(&self, _population: &mut [Individual<T>], _children: &[Individual<T>]) {}
+  fn apply(&self, _population: Vec<Individual<T>>, children: Vec<Individual<T>>) -> Vec<Individual<T>> {
+    children
+  }
+
+  /// Returns `false`.
+  #[inline(always)]
+  fn requires_children_fitness(&self) -> bool {
+    false
+  }
 }
 
 /// # Noop replacement operator
@@ -63,8 +83,6 @@ impl<T: Chromosome> ReplacementOperator<T> for BothParents {
 /// This struct implements [ReplacementOperator] trait and can be used with genetic algorithm.
 ///
 /// It does nothing. Implementation is a noop.
-///
-/// This operator is the same as [BothParents]. Can be treated as an alias.
 pub struct Noop;
 
 impl Noop {
@@ -75,5 +93,12 @@ impl Noop {
 
 impl<T: Chromosome> ReplacementOperator<T> for Noop {
   #[inline(always)]
-  fn apply(&self, _population: &mut [Individual<T>], _children: &[Individual<T>]) {}
+  fn apply(&self, population: Vec<Individual<T>>, _children: Vec<Individual<T>>) -> Vec<Individual<T>> {
+    population
+  }
+
+  #[inline(always)]
+  fn requires_children_fitness(&self) -> bool {
+    false
+  }
 }
