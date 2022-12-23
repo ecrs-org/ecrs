556aa2692080cdfb9cdc992633faced1fe990328
Kacper Kafara <kacperkafara@gmail.com>
2022-12-23 11:38:17 +0100

    Add implementation

--- src/ga/operators/replacement.rs
+++ src/ga/operators/replacement.rs
@@ -4,6 +4,8 @@
 //! original one and the result of crossover phase to a single one,
 //! which will be the next generation
 
+use itertools::Itertools;
+
 use crate::ga::{individual::Chromosome, Individual};
 
 /// # Replacement Operator
@@ -45,7 +47,7 @@ pub trait ReplacementOperator<T: Chromosome> {
 ///
 /// It works simply by replacing parents with their children. In effect, each individual
 /// only gets to breed once.
-/// 
+///
 /// **NOTE**: In current implementation, all library-implemented operators assume that
 /// at indices i, i+1 in `population` collection there are parents of children i, i+1
 /// from `children` collection. Any violation of this invariant may lead to bugs - it can
@@ -113,5 +115,92 @@ impl<T: Chromosome> ReplacementOperator<T> for Noop {
   #[inline(always)]
   fn requires_children_fitness(&self) -> bool {
     false
+  }
+}
+
+/// # WeakParent replacement operator
+///
+/// This struct implements [ReplacementOperator] trait and can be used with genetic algorithm.
+///
+/// Works by taking two out of four individuals (two parents and two children) with the largest fitness.
+///
+/// **NOTE**: In current implementation, all library-implemented operators assume that
+/// at indices i, i+1 in `population` collection there are parents of children i, i+1
+/// from `children` collection. Any violation of this invariant may lead to bugs - it can
+/// be considered an undefined behaviour. We'll work towards improving this case in the future.
+///
+/// **NOTE**: This operator assumes that the size of `population` and `children` are of same size.
+/// Assertion is performed only in debug build. Breaking this condition may lead to bugs and can be thought
+/// of as undefined behaviour.
+///
+/// **NOTE**: This operator assumes that the size of `population` and `children` is a even number.
+/// Assertion is performed only in debug build. Breaking this condition may lead to bugs and can be thought
+/// of as undefined behaviour. This restriction will be removed in future versions
+/// of the library.
+pub struct WeakParent;
+
+impl WeakParent {
+  /// Returns new instance of [WeakParent] replacement operator.
+  pub fn new() -> Self {
+    Self
+  }
+}
+
+impl<T: Chromosome> ReplacementOperator<T> for WeakParent {
+  /// Works by taking two out of four individuals (two parents and two children) with the largest fitness.
+  ///
+  /// **NOTE**: In current implementation, all library-implemented operators assume that
+  /// at indices i, i+1 in `population` collection there are parents of children i, i+1
+  /// from `children` collection. Any violation of this invariant may lead to bugs - it can
+  /// be considered an undefined behaviour. We'll work towards improving this case in the future.
+  ///
+  /// **NOTE**: This operator assumes that the size of `population` and `children` are of same size.
+  /// Assertion is performed only in debug build. Breaking this condition may lead to bugs and can be thought
+  /// of as undefined behaviour.
+  ///
+  /// **NOTE**: This operator assumes that the size of `population` and `children` is a even number.
+  /// Assertion is performed only in debug build. Breaking this condition may lead to bugs and can be thought
+  /// of as undefined behaviour. This restriction will be removed in future versions
+  /// of the library.
+  ///
+  /// ### Arguments
+  ///
+  /// * `population` - Original population, input to the crossover phase.
+  /// This collection should be modified in place by the operator.
+  /// * `children` - Result of the crossover phase
+  fn apply(
+    &self,
+    mut population: Vec<Individual<T>>,
+    mut children: Vec<Individual<T>>,
+  ) -> Vec<Individual<T>> {
+    debug_assert_eq!(
+      population.len(),
+      children.len(),
+      "Population and children must be of the same size"
+    );
+    debug_assert!(population.len() % 2 == 0, "Population size must be even");
+
+    // Unfortunately array windowing is not in stable Rust yet, I believe
+    // https://doc.rust-lang.org/std/slice/struct.ArrayChunks.html
+
+    // There is for sure a nicer way to implement this ;D
+    for i in (0..(population.len() - 1)).step_by(2) {
+      if children[i] > population[i] {
+        std::mem::swap(&mut population[i], &mut children[i]);
+        if children[i + 1] > population[i + 1] {
+          std::mem::swap(&mut population[i + 1], &mut children[i + 1]);
+        }
+      } else if children[i] > population[i + 1] {
+        std::mem::swap(&mut population[i + 1], &mut children[i]);
+        if children[i + 1] > population[i] {
+          std::mem::swap(&mut population[i], &mut children[i + 1]);
+        }
+      } else if children[i + 1] > population[i] {
+        std::mem::swap(&mut population[i], &mut children[i + 1]);
+      } else if children[i + 1] > population[i + 1] {
+        std::mem::swap(&mut population[i + 1], &mut children[i + 1]);
+      }
+    }
+    population
   }
 }
