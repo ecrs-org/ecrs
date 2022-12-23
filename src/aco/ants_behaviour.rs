fa7ec97c0b7251aa4c909a270269935cf72cad34
co012 <co123@interia.eu>
2022-12-23 13:48:26 +0100

    acs ant behavior

--- src/aco/ants_behaviour.rs
+++ src/aco/ants_behaviour.rs
@@ -57,3 +57,53 @@ impl<A: Ant, G: Goodness> AntsBehaviour<A, G> for AntSystemAB {
     paths
   }
 }
+
+/// # Ant Colony System ants behaviour
+///
+/// Implements [AntsBehaviour]. Ants are simulated as described in Ant Colony System algorithm with the
+/// exception of goodness calculation.
+pub struct AntColonySystemAB;
+
+impl<A: Ant, G: Goodness> AntsBehaviour<A, G> for AntColonySystemAB {
+  fn simulate_ants(
+    &mut self,
+    ants: &mut [A],
+    pheromone: &mut FMatrix,
+    goodness_op: &mut G,
+  ) -> Vec<Vec<usize>> {
+    let solution_size = pheromone.nrows();
+
+    ants.iter_mut().for_each(|a| {
+      a.clear();
+      a.chose_staring_place()
+    });
+
+    let mut paths: Vec<Vec<usize>> = Vec::with_capacity(ants.len());
+
+    for _ in 1..solution_size {
+      let goodness = goodness_op.apply(pheromone);
+      paths.clear();
+      for ant in ants.iter_mut() {
+        if ant.is_stuck() {
+          continue;
+        }
+        ant.go_to_next_place(&goodness);
+
+        let path = ant.path();
+        paths.push(path.to_vec())
+      }
+      local_update(pheromone, &paths)
+    }
+    paths
+  }
+}
+
+fn local_update(pheromone: &mut FMatrix, partial_paths: &[Vec<usize>]) {
+  for p_path in partial_paths {
+    let l = p_path.len();
+    let s = p_path[l - 2];
+    let r = p_path[l - 1];
+
+    pheromone[(s, r)] *= 0.9
+  }
+}
