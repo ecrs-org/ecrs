27e21d3d47ba4fd10337486d35e59e1f02c3a20b
Kacper Kafara <kacperkafara@gmail.com>
2023-01-06 19:18:59 +0100

    Docs for generic builder

--- src/ga/builder/generic.rs
+++ src/ga/builder/generic.rs
@@ -1,3 +1,5 @@
+//! Fully configurable builder implementation for any type of chromosome
+
 use crate::ga::builder::FitnessFn;
 use crate::ga::operators::fitness::{Fitness, FnBasedFitness};
 use crate::ga::operators::replacement::ReplacementOperator;
@@ -10,6 +12,8 @@ use crate::ga::{
 
 use super::{DefaultParams, GAConfigOpt};
 
+/// [GenericBuilder] must be fully configured. Only defaults for params are provided,
+/// altough it is preffered to override them.
 pub struct GenericBuilder<T, M, C, S, R, P, F, Pr>
 where
   T: Chromosome,
@@ -50,76 +54,158 @@ where
   F: Fitness<T>,
   Pr: Probe<T>,
 {
+  /// Returns new instance of [GenericBuilder]
   pub fn new() -> Self {
     GenericBuilder {
       config: GAConfigOpt::new(),
     }
   }
 
+  /// Sets selection rate
+  ///
+  /// ## Arguments
+  ///
+  /// * `selection_rate` - Selection rate; must be in [0, 1] interval
+  ///
+  /// ## Panics
+  ///
+  /// If the `selection_rate` param has invalid value.
   pub fn set_selection_rate(mut self, selection_rate: f64) -> Self {
-    debug_assert!((0f64..=1f64).contains(&selection_rate));
+    assert!((0f64..=1f64).contains(&selection_rate));
     self.config.params.selection_rate = Some(selection_rate);
     self
   }
 
+  /// Sets mutation rate
+  ///
+  /// ## Arguments
+  ///
+  /// * `mutation_rate` - Mutation rate; must be in [0, 1] interval
+  ///
+  /// ## Panics
+  ///
+  /// If the parameter has invalid value.
   pub fn set_mutation_rate(mut self, mutation_rate: f64) -> Self {
     assert!((0.0..=1.0).contains(&mutation_rate));
     self.config.params.mutation_rate = Some(mutation_rate);
     self
   }
 
+  /// Sets max duration. If exceeded, the algorithm halts.
+  ///
+  /// ## Arguments
+  ///
+  /// * `max_duration` - Maximal duration of computations
   pub fn set_max_duration(mut self, max_duration: std::time::Duration) -> Self {
     self.config.params.max_duration = Some(max_duration);
     self
   }
 
+  /// Sets maximal generation count. If exceeded, the algorithm halts.
+  ///
+  /// ## Arguments
+  ///
+  /// * `max_gen_count` - Maximal number of generations to conduct computations for.
+  ///
+  /// ## Panics
+  ///
+  /// If the parameter has invalid value.
   pub fn set_max_generation_count(mut self, max_gen_count: usize) -> Self {
-    debug_assert!(max_gen_count >= 1);
+    assert!(max_gen_count >= 1);
     self.config.params.generation_limit = Some(max_gen_count);
     self
   }
 
+  /// Sets population size. Must be positive.
+  ///
+  /// ## Arguments
+  ///
+  /// * `size` - Strength of population (number of individuals)
+  ///
+  /// ## Panics
+  ///
+  /// If the parameter has invalid value.
   pub fn set_population_size(mut self, size: usize) -> Self {
-    debug_assert!(size > 0);
+    assert!(size > 0);
     self.config.params.population_size = Some(size);
     self
   }
 
+  /// Sets fitness
+  ///
+  /// ## Arguments
+  ///
+  /// * `fitness` - Structure implementing the Fitness trait. See its documentation for details.
   pub fn set_fitness(mut self, fitness: F) -> Self {
     self.config.fitness_fn = Some(fitness);
     self
   }
 
+  /// Sets mutation operator
+  ///
+  /// ## Arguments
+  ///
+  /// * `mutation_op` - struct implementing [MutationOperator](crate::ga::operators::mutation::MutationOperator) trait
   pub fn set_mutation_operator(mut self, mutation_op: M) -> Self {
     self.config.mutation_operator = Some(mutation_op);
     self
   }
 
+  /// Sets crossover operator
+  ///
+  /// ## Arguments
+  ///
+  /// * `crossover_op` - struct implementing [CrossoverOperator](crate::ga::operators::crossover::CrossoverOperator) trait
   pub fn set_crossover_operator(mut self, crossover_op: C) -> Self {
     self.config.crossover_operator = Some(crossover_op);
     self
   }
 
+  /// Sets selection operator
+  ///
+  /// ## Arguments
+  ///
+  /// * `selection_op` - struct implementing [SelectionOperator](crate::ga::operators::selection::SelectionOperator) trait
   pub fn set_selection_operator(mut self, selection_op: S) -> Self {
     self.config.selection_operator = Some(selection_op);
     self
   }
 
+  /// Sets replacement operator
+  ///
+  /// ## Arguments
+  ///
+  /// * `replacement_op` - struct implementing [ReplacementOperator](crate::ga::operators::replacement::ReplacementOperator) trait
   pub fn set_replacement_operator(mut self, replacement_op: R) -> Self {
     self.config.replacement_operator = Some(replacement_op);
     self
   }
 
+  /// Sets population generator
+  ///
+  /// ## Arguments
+  ///
+  /// * `generator` - struct implementing [PopulationGenerator](crate::ga::population::PopulationGenerator) trait
   pub fn set_population_generator(mut self, generator: P) -> Self {
     self.config.population_factory = Some(generator);
     self
   }
 
+  /// Sets probe
+  ///
+  /// ## Arguments
+  ///
+  /// * `probe` - struct implementing [Probe](crate::ga::probe::Probe) trait
   pub fn set_probe(mut self, probe: Pr) -> Self {
     self.config.probe = Some(probe);
     self
   }
 
+  /// Returns new instance of [GeneticAlgorithm](crate::ga::GeneticAlgorithm)
+  ///
+  /// ## Panics
+  ///
+  /// Iff any of the parameters has invalid value.
   pub fn build(mut self) -> GeneticAlgorithm<T, M, C, S, R, P, F, Pr> {
     self.config.params.fill_from(&Self::DEFAULT_PARAMS);
 
