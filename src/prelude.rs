7ac6d2900fec4c7885de541656c754fc53396ed5
co012 <co123@interia.eu>
2023-04-26 23:14:57 +0200

    hide algorithms behind features

--- src/prelude.rs
+++ src/prelude.rs
@@ -6,11 +6,18 @@
 //! use rand::prelude::*;
 //! ```
 
+#[cfg(feature = "aco")]
 pub use crate::aco;
+#[cfg(feature = "ff")]
 pub use crate::ff;
+#[cfg(feature = "ga")]
 pub use crate::ga;
+#[cfg(feature = "ga")]
 pub use crate::ga::operators as ops;
+#[cfg(feature = "ga")]
 pub use crate::ga::operators::{crossover, fitness, mutation, replacement, selection};
+#[cfg(feature = "ga")]
 pub use crate::ga::population;
+#[cfg(feature = "pso")]
 pub use crate::pso;
 pub use crate::test_functions as tf;
