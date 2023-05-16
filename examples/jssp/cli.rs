c16ede3d81901d859f1d0c650e5f7d917777eb0c
Kacper Kafara <kacperkafara@gmail.com>
2023-05-16 11:08:29 +0200

    Validate passed paths

--- examples/jssp/cli.rs
+++ examples/jssp/cli.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 use log::error;
 
 
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, Clone)]
 pub struct Args {
     /// Path to the data directory
     #[arg(short, long)]
@@ -14,15 +14,26 @@ pub struct Args {
     file: Option<PathBuf>,
 }
 
+fn validate_args(args: &Args) -> Result<(), String> {
+    let cloned_args = args.clone(); 
+    if args.file.is_some() {
+        if !cloned_args.file.unwrap().is_file() {
+            return Err("Specified data file does not exist or is not a file".to_owned());
+        }
+    }
+
+    if args.data_dir.is_some() {
+        if !cloned_args.data_dir.unwrap().is_dir() {
+            return Err("Specified data directory does not exist or is not a directory".to_owned());
+        }
+    }
+    Ok(())
+}
+
 pub fn parse_args() -> Args {
     let args = Args::parse();     
     if validate_args(&args).is_err() {
         error!("Validation of the cli args failed");
     }
     args
-}
-
-fn validate_args(args: &Args) -> Result<(), String> {
-    // No validation is required for now
-    Ok(())
 }
