bbb8784ec32fe62d31fb2b7545d05d4c732eddcd
Kacper Kafara <kacperkafara@gmail.com>
2023-05-25 22:45:55 +0200

    Make generation tools public

--- src/ga/population/tools.rs
+++ src/ga/population/tools.rs
@@ -8,25 +8,26 @@ pub struct PointGenerator<R: Rng = ThreadRng> {
 }
 
 impl PointGenerator<ThreadRng> {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self::with_rng(thread_rng())
     }
 }
 
 impl<R: Rng + Clone> PointGenerator<R> {
-    fn with_rng(rng: R) -> Self {
+    pub fn with_rng(rng: R) -> Self {
         Self { rng }
     }
 
-    fn generate(&self, dim: usize, n: usize) -> Vec<Vec<f64>> {
+    pub fn generate(&self, dim: usize, n: usize) -> Vec<Vec<f64>> {
         self.generate_with_single_constraint(dim, n, 0.0..1.0)
     }
 
-    fn generate_with_single_constraint(&self, dim: usize, n: usize, constraint: Range<f64>) -> Vec<Vec<f64>> {
+    pub fn generate_with_single_constraint(&self, dim: usize, n: usize, constraint: Range<f64>) -> Vec<Vec<f64>> {
         self.generate_with_constraints(dim, n, Vec::from_iter(std::iter::repeat(constraint)))
     }
 
-    fn generate_with_constraints(&self, dim: usize, n: usize, constraints: Vec<Range<f64>>) -> Vec<Vec<f64>> {
+    pub fn generate_with_constraints(&self, dim: usize, n: usize, constraints: Vec<Range<f64>>) -> Vec<Vec<f64>> {
+        assert_eq!(n, constraints.len());
         Vec::from_iter(constraints.iter().map(|constr| {
             self.rng
                 .clone()
