cff58138069feb5cc3d6ed5f63bc8a6dd8cc0aac
Kacper Kafara <kacperkafara@gmail.com>
2023-10-09 19:07:55 +0200

    Assert that output directory exists

--- examples/jssp/main.rs
+++ examples/jssp/main.rs
@@ -58,7 +58,8 @@ fn run_with_ecrs(instance: JsspInstance, _args: Args) {
 
 fn run() {
     let args = cli::parse_args();
-
+    
+    util::assert_dir_exists(args.output_dir.as_ref());
     let event_map = util::create_event_map(args.output_dir.as_ref());
     if let Err(err) = logging::init_logging(&event_map) {
         panic!("Logger initialization failed with error: {err}");
--- examples/jssp/util.rs
+++ examples/jssp/util.rs
@@ -26,3 +26,14 @@ pub fn create_event_map(base_dir: &Path) -> HashMap<String, PathBuf> {
         ("iterinfo".to_owned(), base_dir.join("event_iterinfo.csv"))
     ])
 }
+
+pub fn assert_dir_exists(dir: &Path) {
+    if dir.is_dir() {
+        return;
+    }
+
+    match std::fs::create_dir_all(dir) {
+        Ok(()) => return,
+        Err(err) => panic!("Failed to create outuput directory with error {err}"),
+    };
+}
