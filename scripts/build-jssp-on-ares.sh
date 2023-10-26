69ae202a9f4550240e5abab83444fc6147636236
Kacper Kafara <kacperkafara@gmail.com>
2023-10-26 22:56:11 +0200

    Add minimal logging to building script

--- scripts/build-jssp-on-ares.sh
+++ scripts/build-jssp-on-ares.sh
@@ -3,11 +3,13 @@
 # $1: module name
 function load_module_if_needed() {
   module_name=$1
+  echo "Adding module ${module_name}"
   if [[ $(module is-loaded ${module_name}) -ne 0 ]]; then
     module add ${module_name}
   fi
 }
 
+echo "Building jssp [release]"
 load_module_if_needed rust/1.65.0-gcccore-12.2.0
 cargo build --example jssp --release
 mkdir -p bin
