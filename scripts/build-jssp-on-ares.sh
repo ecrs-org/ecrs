b6efffaef3bcc02b72af1b5d18a1afa5bccf86e8
Kacper Kafara <kacperkafara@gmail.com>
2023-10-26 12:42:35 +0200

    Add script for building jssp binary on Ares

---
+++ scripts/build-jssp-on-ares.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+# $1: module name
+function load_module_if_needed() {
+  module_name=$1
+  if [[ $(module is-loaded ${module_name}) -ne 0 ]]; then
+    module add ${module_name}
+  fi
+}
+
+load_module_if_needed rust/1.65.0-gcccore-12.2.0
+cargo build --example jssp --release
+mkdir -p bin
+mv target/release/examples/jssp bin/
+
