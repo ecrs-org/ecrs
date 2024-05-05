9daa62ee85ba4f9faa628d35b50e78fb91de25a0
Kacper Kafara <kacperkafara@gmail.com>
2024-05-05 15:18:50 +0200

    Add population size to metrics

--- src/ga.rs
+++ src/ga.rs
@@ -151,6 +151,14 @@ pub struct GAParams {
     pub max_duration: std::time::Duration,
 }
 
+pub struct Telemetry {
+
+}
+
+pub struct Instrumentation {
+    pub telemetry: Telemetry,
+}
+
 pub struct GAConfig<IndividualT, MutOpT, CrossOpT, SelOpT, ReplOpT, PopGenT, FitnessT, ProbeT>
 where
     IndividualT: IndividualTrait,
@@ -176,6 +184,7 @@ where
 #[derive(Default)]
 pub struct Metrics {
     pub generation: usize,
+    pub population_size: usize,
     pub start_time: Option<std::time::Instant>,
 
     /// This field can not be relied upon. It is updated only in the begining
@@ -196,9 +205,11 @@ impl Metrics {
         start_time: Option<std::time::Instant>,
         duration: Option<std::time::Duration>,
         generation: usize,
+        population_size: usize,
     ) -> Self {
         Metrics {
             generation,
+            population_size,
             start_time,
             total_dur: duration,
             pop_gen_dur: None,
@@ -243,9 +254,10 @@ where
     pub fn new(
         config: GAConfig<IndividualT, MutOpT, CrossOpT, SelOpT, ReplOpT, PopGenT, FitnessT, ProbeT>,
     ) -> Self {
+        let population_size = config.params.population_size;
         GeneticSolver {
             config,
-            metrics: Metrics::new(None, None, 0),
+            metrics: Metrics::new(None, None, 0, population_size),
             timer: Timer::new(),
         }
     }
@@ -277,6 +289,8 @@ where
         self.timer.start();
         let mut population = self.gen_pop();
         self.metrics.pop_gen_dur = Some(self.timer.elapsed());
+
+        self.metrics.population_size = population.len();
 
         self.timer.start();
         self.eval_pop(&mut population);
@@ -335,6 +349,8 @@ where
                 .replacement_operator
                 .apply(&self.metrics, population, children);
             self.metrics.replacement_dur = Some(self.timer.elapsed());
+
+            self.metrics.population_size = population.len();
 
             assert_eq!(population.len(), self.config.params.population_size,
                 "There was change in population size from {} to {} in generation {}. Dynamic population size is currently not supported.",
